6c40daaf68d1d926bd9bce1cd37940aaa3448694
Ilkin Mehdiyev <hey@ilkinmehdiyev.com>
2022-12-05 20:16:50 +0400

    Init commit

---
+++ src/main/java/com/ilkinmehdiyev/dataorb_pps_task/DataorbPpsTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ilkinmehdiyev.dataorb_pps_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataorbPpsTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataorbPpsTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilkinmehdiyev/dataorb_pps_task/DataorbPpsTaskApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ilkinmehdiyev.dataorb_pps_task;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataorbPpsTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
